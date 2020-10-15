0040fbb113856cdfb82ebf36cf19c707e70f0804
GabrielMorais99 <gabrielmorais80@hotmail.com>
2020-10-15 01:45:59 -0300

    comentários das alterações

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import Tmdb from './Tmdb'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default () => {
+	useEffect(() => {
+		const loadAll = async () => {
+			// Pegando a lista totall
+
+			let list = await Tmdb.getHomeList()
+
+			console.log(list)
+		}
+		loadAll()
+	}, [])
+
+	return <div>Olá mundo</div>
 }
-
-export default App;
